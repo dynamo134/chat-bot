aa792bca67231c13b558c85083dd26e825b12e0c
dynamo134 <shaharyaramu16@gmail.com>
2024-04-24 18:25:46 +0530

    Frontend SetUp

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,10 +1,10 @@
-import User from "../models/user.model.js";
+import User from "../models/user.models.js";
 
 export const getUsersForSidebar = async (req, res) => {
 	try {
 		const loggedInUserId = req.user._id;
 
-		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");  //find all the user except yourself
 
 		res.status(200).json(filteredUsers);
 	} catch (error) {
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Login from './pages/login/login'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+			<Login/>
+		</div>
+  )
+}
+
+export default App
